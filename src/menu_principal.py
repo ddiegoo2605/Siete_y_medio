26ad4fdacb4ca8c083798a782cca97fb6e607034
ddiegoo2605 <diego.e.aguilar.ojeda@gmail.com>
2025-01-05 21:56:13 +0100

    añadir funcion imprimir titulos

--- src/menu_principal.py
+++ src/menu_principal.py
@@ -2,15 +2,31 @@ import jugadores
 
 def menu_principal():
     while True:
-        print("\n ---------- MENÚ PRINCIPAL ----------")
-        print("1. Gestión de jugadores")
-        print("2. Ajustes")
-        print("3. Jugar")
-        print("4. Estadísticas")
-        print("5. Reportes")
-        print("6. Salir")
+        titulo = "JUEGO SIETE Y MEDIO"
+        largo_titulo = len(titulo)
+        margen = 20 
+        largo_linea = largo_titulo + margen * 2 
+        espacio = " " * margen
 
-        seleccion = input("Selecciona una opción (1-6): ")
+        print("\n" + "-" * largo_linea)
+        print("-" * margen + titulo.center(largo_linea - margen * 2) + "-" * margen)
+        print("-" * largo_linea)
+        print("\n"+ espacio + "MENÚ PRINCIPAL")
+        print("\n")
+        opciones = [
+            "1. Gestión de jugadores",
+            "2. Ajustes",
+            "3. Jugar",
+            "4. Estadísticas",
+            "5. Reportes",
+            "6. Salir"
+        ]
+        
+        for opcion in opciones:
+            print(f"{espacio}{opcion}")
+        
+
+        seleccion = input("\n"+ espacio +"Selecciona una opción (1-6): ")
 
         if seleccion == '1':
            jugadores.gestion_jugadores()
@@ -23,7 +39,8 @@ def menu_principal():
         elif seleccion == '5':
             print("Reportes. Todavia falta por implementar")
         elif seleccion == '6':
-            print("Gracias por jugar, hasta la proxima")
+            print("\n"+"Gracias por jugar, hasta la próxima")
+            print("\n"+ "#" * largo_linea)
             break
         else:
             print("Error. Escoge una opción valida entre (1-6)")
