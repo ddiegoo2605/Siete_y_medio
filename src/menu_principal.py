741e673990141125959ec53c0c74c1e3de9bc7a1
ddiegoo2605 <diego.e.aguilar.ojeda@gmail.com>
2025-01-21 02:39:31 +0100

    Squashed commit of the following:
    commit 38134ac7790cee0c434de924a2a2d088f2286492
    Merge: 13e611d 1765a10
    Author: Yeray2353 <yeray28diciembre@gmail.com>
    Date:   Tue Jan 21 02:25:39 2025 +0100
    
        Merge branch 'Diego' into Yeray
    
    commit 13e611de5dfffe90a47ca1c13e16fb73dfec84cb
    Merge: 5f369a2 2e9f255
    Author: Yeray2353 <yeray28diciembre@gmail.com>
    Date:   Tue Jan 21 02:24:20 2025 +0100
    
        Update menu_principal.py
    
    commit 5f369a245e23cdac17a3cf54878b97aae2326eff
    Merge: 6e54afe 03b572b
    Author: Yeray2353 <yeray28diciembre@gmail.com>
    Date:   Tue Jan 21 02:22:26 2025 +0100
    
        Merge branch 'Preproducción' into Yeray
    
    commit 6e54afe7b8bc9a928232728e99d4f711b38577e3
    Merge: ac48cf4 83c22b0
    Author: Yeray2353 <yeray28diciembre@gmail.com>
    Date:   Tue Jan 21 02:21:55 2025 +0100
    
        Merge branch 'Yeray' of https://github.com/ddiegoo2605/Siete_y_medio into Yeray
    
    commit ac48cf4e38e6a51df2206b45327ecbe50b288aff
    Author: Yeray2353 <yeray28diciembre@gmail.com>
    Date:   Tue Jan 21 02:21:49 2025 +0100
    
        Pequeños cambios para sacar los jugadores de la bdd
    
    commit 2e9f255d45a1bb73bb8c0e4906dc33b034ed5111
    Author: GerardCollazos <gerard.cl11122004@gmail.com>
    Date:   Mon Jan 20 03:40:32 2025 +0100
    
        menu_principal.py
    
    commit 546e4aa2cce6169554b828bb5fdbe4ced08c3a84
    Merge: 1a0e09b 8b48c3e
    Author: ddiegoo2605 <diego.e.aguilar.ojeda@gmail.com>
    Date:   Sun Jan 19 11:23:45 2025 +0100
    
        Merge pull request #42 from ddiegoo2605/Preproducción
    
        Preproducción

--- src/menu_principal.py
+++ src/menu_principal.py
@@ -266,84 +266,35 @@ def new_boot():
             print("opción no válida. Introduce 'y' o 'n'.")
 
 def show_players():
-    dnis_bots = []
-    dnis = []
+    botardo = {}
+    jugadorazos = {}
     cursor = conexion.conexion.cursor()
-    cursor.execute("SELECT id, nombre, tipo FROM jugadores")
+    cursor.execute("SELECT id, nombre,tipo FROM bots")
     usuarios = cursor.fetchall()
-    for id in usuarios:
-        dnis_bots += [id,]
-    print(dnis_bots)
+    for id, nombre, tipo in usuarios:
+        if tipo == 50:
+            tipo = "Ambicious"
+        elif tipo == 40:
+            tipo = "Moderated"
+        elif tipo == 30:
+            tipo = "Bold"
+        botardo += {id:{"nombre":nombre , "tipo": tipo}}
+    cursor.execute("SELECT id, nombre, nif, tipo FROM jugadores")
+    usuarios = cursor.fetchall()
+    for id, nombre, nif, tipo in usuarios:
+        if tipo == 50:
+            tipo = "Ambicious"
+        elif tipo == 40:
+            tipo = "Moderated"
+        elif tipo == 30:
+            tipo = "Bold"
+        
+        jugadorazos += {nif:{"nombre":nombre , "tipo": tipo}}
+    if len(jugadorazos) > len(botardo):
+        for i in range(len(jugadorazos)):
+            if i <= len(botardo):
+                print(f"{}. {botardo[i]['nombre']} - {botardo[i]['tipo']}")
     
-    if len(jugadores.jugadores) < len(jugadores.bots):
-       tamano = len(jugadores.bots)
-    elif len(jugadores.jugadores) > len(jugadores.bots):
-        tamano = len(jugadores.jugadores)
-    else:
-        tamano = len(jugadores.jugadores) 
-    for i in range(tamano):
-        if len(jugadores.bots) > len(jugadores.jugadores):
-            
-            if i >= len(jugadores.jugadores):
-                if jugadores.bots[dnis_bots[i]]["type"] == 50:
-                    type_bot = "Ambicious"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 40:
-                    type_bot = "Moderated"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 30:
-                    type_bot = "Bold"
-                print(f"{dnis_bots[i]}" +f" {jugadores.bots[dnis_bots[i]]['name']} {type_bot} ||")
-            else:
-                if jugadores.bots[dnis_bots[i]]["type"] == 50:
-                    type_bot = "Ambicious"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 40:
-                    type_bot = "Moderated"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 30:
-                    type_bot = "Bold"
-                if jugadores.jugadores[dnis[i]]["type"] == 50:
-                    type = "Ambicious"
-                elif jugadores.jugadores[dnis[i]]["type"] == 40:
-                    type = "Moderated"
-                elif jugadores.jugadores[dnis[i]]["type"] == 30:
-                    type = "Bold"
-                print(f"{dnis_bots[i]} {jugadores.bots[dnis_bots[i]]['name']} {type_bot} || {dnis[i]} {jugadores.jugadores[dnis[i]]['name']} {type} ")
-
-        elif len(jugadores.bots) < len(jugadores.jugadores):
-            if i >= len(jugadores.bots):
-                if jugadores.jugadores[dnis[i]]["type"] == 50:
-                    type = "Ambicious"
-                elif jugadores.jugadores[dnis[i]]["type"] == 40:
-                    type = "Moderated"
-                elif jugadores.jugadores[dnis[i]]["type"] == 30:
-                    type = "Bold"
-                print(f"{dnis[i]}" +f"{jugadores.jugadores[dnis[i]]['name']} {type}")
-            else:
-                if jugadores.bots[dnis_bots[i]]["type"] == 50:
-                    type_bot = "Ambicious"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 40:
-                    type_bot = "Moderated"
-                elif jugadores.bots[dnis_bots[i]]["type"] == 30:
-                    type_bot = "Bold"
-                if jugadores.jugadores[dnis[i]]["type"] == 50:
-                    type = "Ambicious"
-                elif jugadores.jugadores[dnis[i]]["type"] == 40:
-                    type = "Moderated"
-                elif jugadores.jugadores[dnis[i]]["type"] == 30:
-                    type = "Bold"
-                print(f"{dnis_bots[i]} {jugadores.bots[dnis_bots[i]]['name']} {type_bot} || {dnis[i]} {jugadores.jugadores[dnis[i]]['name']} {type} ")
-        else:
-            if jugadores.bots[dnis_bots[i]]["type"] == 50:
-                type_bot = "Ambicious"
-            elif jugadores.bots[dnis_bots[i]]["type"] == 40:
-                type_bot = "Moderated"
-            elif jugadores.bots[dnis_bots[i]]["type"] == 30:
-                type_bot = "Bold"
-            if jugadores.jugadores[dnis[i]]["type"] == 50:
-                type = "Ambicious"
-            elif jugadores.jugadores[dnis[i]]["type"] == 40:
-                type = "Moderated"
-            elif jugadores.jugadores[dnis[i]]["type"] == 30:
-                type = "Bold"
-            print(f"{dnis_bots[i]} {jugadores.bots[dnis_bots[i]]['name']} {type_bot} || {dnis[i]} {jugadores.jugadores[dnis[i]]['name']} {type} ")
 
 #FALTA IMPLEMENTAR UNA FUNCION QUE ELIMINE LOS JUGADORES
 
@@ -379,6 +330,7 @@ def set_game_players():
 #   TERCERA OPCIÓN
 
 def playGame():
+    ask_card = False
     set_cards = False
     if set_cards == False:
         print("Set cards first")
@@ -403,9 +355,24 @@ def playGame():
         elif num_opcion == 2:
             print("Aún no implementado")
         elif num_opcion == 3:
+<<<<<<< HEAD
             print("Aún no implementado")
         elif num_opcion == 4:
             print("Aún no implementado")
+=======
+            if ask_card:
+                print("You're not allowed to change the bet if you have ordered some card.")
+                input("Enter to continue")
+            else:
+                apuesta_personalizada = int(input("Set the new Bet: "))
+                if 1 <= apuesta_personalizada <= jugador["puntos"]:
+                    jugador["apuesta"] = apuesta_personalizada
+                    input("Enter to continue")
+                else:
+                    print(f"The New Bet has to be a number between 1 and {jugador['puntos']}.")
+        elif num_opcion == 4:
+            ask_card = True
+>>>>>>> main
         elif num_opcion == 5:
             print("Aún no implementado")
         elif num_opcion == 6:
@@ -675,4 +642,7 @@ def agregar_jugador_bdd(jugador,nif,quien = ""):
 
     
 menu_principal()
+<<<<<<< HEAD
 
+=======
+>>>>>>> main
